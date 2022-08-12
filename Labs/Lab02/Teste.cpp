aa1a7a724edde87b36192644364bb2974ddd4926
Judson Santiago <judson@ufersa.edu.br>
2022-08-12 10:29:14 -0300

    Comentários adicionados
    Para deixar mais claro como o programa de teste está ilustrando os conceitos vistos na aula.

--- Labs/Lab02/Teste.cpp
+++ Labs/Lab02/Teste.cpp
@@ -1,15 +1,17 @@
 #include "Jogo.h"
 
 int main()
-{
+{	
+	// variável do tipo Jogo
 	Jogo gow;
 
-	gow.adquirir("Gow", 160.0f);
-	gow.exibir();
+	// chamando métodos 
+	gow.adquirir("Gow", 160.0f);    // altera atributos do jogo
+	gow.exibir();					// exibe os atributos do jogo
 
-	gow.jogar(5);
-	gow.exibir();
+	gow.jogar(5);					// altera horas jogadas
+	gow.exibir();					// exibe os dados
 
-	gow.jogar(3);
-	gow.exibir();
+	gow.jogar(3);					// adiciona mais 3 horas jogadas
+	gow.exibir();					// exibe o jogo
 }
